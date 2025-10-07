9b050d65b39e217eee72178ed67cf53f32b760dc
jieunheo <gjekdmlwl@naver.com>
2025-10-07 20:35:42 +0900

    지도

---
+++ new-app/src/Components/Main/Map.jsx
@@ -0,0 +1,9 @@
+import { useEffect, useRef, useState } from "react";
+import styled from "styled-components";
+const { kakao, naver } = window;
+
+const MapDiv = styled.section``;
+
+export default function Map() {
+  return <MapDiv>지도</MapDiv>;
+}
