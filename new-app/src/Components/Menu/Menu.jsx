f4693ab34c94582e19f91b9d67760075ee101219
jieunheo <gjekdmlwl@naver.com>
2025-10-09 22:57:18 +0900

    메뉴 페이지

--- new-app/src/Components/Menu/Menu.jsx
+++ new-app/src/Components/Menu/Menu.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import styled from "styled-components";
 
 const MenuDiv = styled.main`
@@ -79,7 +80,8 @@ const MenuDiv = styled.main`
     width: 100%;
 
     display: grid;
-    grid-template-columns: 1fr 1fr 1fr 1fr;
+    // grid-template-columns: 1fr 1fr 1fr 1fr;
+    grid-template-columns: repeat(4, minmax(232px, 1fr));
     gap: 18px;
   }
   .menu-item {
@@ -92,6 +94,12 @@ const MenuDiv = styled.main`
     display: flex;
     flex-direction: column;
     gap: 10px;
+
+    transition: 0.2s;
+    cursor: pointer;
+  }
+  .menu-item:hover {
+    transform: translateY(-2%);
   }
   .menu-item img {
     width: 100%;
@@ -129,7 +137,71 @@ const MenuDiv = styled.main`
   }
 `;
 
+const titleArray = {
+  main: "메인",
+  meat: "고기류",
+  soup: "국물류",
+  etc: "기타",
+  noodle: "면류",
+  rice: "밥류",
+  alcohol: "주류",
+  Makgeolli: "막걸리",
+  drink: "음료",
+};
+
 export default function Menu() {
+  const [allMenus, setAllMenus] = useState(null);
+  const [viewMenus, setViewMenus] = useState([]);
+  const [currentMenu, setCurrentMenu] = useState("main");
+  const [cates, setCates] = useState([]);
+  const [loading, setLoading] = useState(true);
+  console.log(viewMenus);
+
+  useEffect(() => {
+    async function getData() {
+      try {
+        const res = await fetch(
+          "http://heoganaen.dothome.co.kr/api/getMenu.php"
+        );
+        if (!res.ok)
+          throw new Error("데이터를 가져오는 중 문제가 발생했습니다.");
+        const data = await res.json();
+
+        const allMenuData = data.data.sort((a, b) => {
+          return a.order - b.order;
+        });
+
+        setAllMenus(allMenuData);
+        setLoading(false);
+        settingMenu(allMenuData);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+
+    getData();
+  }, [currentMenu]);
+
+  function settingMenu(allMenuData) {
+    let datas;
+    if (allMenus) datas = allMenus;
+    else datas = allMenuData;
+
+    let category = [];
+    const filter = datas.filter((item) => {
+      if (item.type === currentMenu) {
+        category = category ? [...category, item.sub] : [item.sub];
+        return true;
+      }
+    });
+    setCates([...new Set(category)]);
+    setViewMenus([...filter]);
+  }
+
+  function handleMenu(type) {
+    setCurrentMenu(type);
+  }
+
   return (
     <MenuDiv>
       <section className="title-wrap">
@@ -143,174 +215,68 @@ export default function Menu() {
         <h3 className="a11y-hidden">메뉴 선택</h3>
         <ul className="tab-list">
           <li className="tab-item ">
-            <button className="active">메인</button>
+            <button
+              className={currentMenu === "main" ? "active" : ""}
+              onClick={() => handleMenu("main")}
+            >
+              메인
+            </button>
           </li>
           <li className="tab-item">
-            <button>후식</button>
+            <button
+              className={currentMenu === "dessert" ? "active" : ""}
+              onClick={() => handleMenu("dessert")}
+            >
+              후식
+            </button>
           </li>
           <li className="tab-item">
-            <button>음료</button>
+            <button
+              className={currentMenu === "drink" ? "active" : ""}
+              onClick={() => handleMenu("drink")}
+            >
+              음료
+            </button>
           </li>
         </ul>
-
-        <div className="drink">
-          <h4 className="menu-title">대표</h4>
-          <ul className="menu-list">
-            <li className="menu-item">
-              <div>
-                <img src="../../imgs/mm01.jpg" alt="" />
+        {loading ? (
+          <div>로딩 중...</div>
+        ) : (
+          <>
+            {cates.map((cate) => (
+              <div className={currentMenu === "drink" ? "drink" : ""}>
+                <h4 className="menu-title">{titleArray[cate]}</h4>
+                <ul className="menu-list">
+                  {viewMenus.map((item) => {
+                    if (item.sub === cate)
+                      return (
+                        <li className="menu-item" key={item.id}>
+                          {item.url && (
+                            <div>
+                              <img src={item.url} alt={item.name} />
+                            </div>
+                          )}
+                          <div className="info-wrap">
+                            <p className="menu-name">
+                              <strong>{item.name}</strong>
+                              {item.desc && (
+                                <span className="ex">({item.desc})</span>
+                              )}
+                            </p>
+                            <p className="menu-name-en">{item["en_name"]}</p>
+                            <hr className="gray-line" />
+                            <p className="menu-price">
+                              <data value={item.price}>{+item.price}</data>원
+                            </p>
+                          </div>
+                        </li>
+                      );
+                  })}
+                </ul>
               </div>
-              <div className="info-wrap">
-                <p className="menu-name">
-                  <strong>무뼈닭발</strong>
-                </p>
-                <p className="menu-name-en">boneless chicken feet</p>
-                <hr className="gray-line" />
-                <p className="menu-price">
-                  <data value="13000">13,000</data>원
-                </p>
-              </div>
-            </li>
-            <li className="menu-item">
-              <div>
-                <img src="../../imgs/mm01.jpg" alt="" />
-              </div>
-              <div className="info-wrap">
-                <p className="menu-name">
-                  <strong>무뼈닭발</strong>
-                </p>
-                <p className="menu-name-en">boneless chicken feet</p>
-                <hr className="gray-line" />
-                <p className="menu-price">
-                  <data value="13000">13,000</data>원
-                </p>
-              </div>
-            </li>
-            <li className="menu-item">
-              <div>
-                <img src="../../imgs/mm01.jpg" alt="" />
-              </div>
-              <div className="info-wrap">
-                <p className="menu-name">
-                  <strong>무뼈닭발</strong>
-                </p>
-                <p className="menu-name-en">boneless chicken feet</p>
-                <hr className="gray-line" />
-                <p className="menu-price">
-                  <data value="13000">13,000</data>원
-                </p>
-              </div>
-            </li>
-            <li className="menu-item">
-              <div>
-                <img src="../../imgs/mm01.jpg" alt="" />
-              </div>
-              <div className="info-wrap">
-                <p className="menu-name">
-                  <strong>무뼈닭발</strong>
-                </p>
-                <p className="menu-name-en">boneless chicken feet</p>
-                <hr className="gray-line" />
-                <p className="menu-price">
-                  <data value="13000">13,000</data>원
-                </p>
-              </div>
-            </li>
-          </ul>
-        </div>
-
-        <div>
-          <h4 className="menu-title">고기</h4>
-          <ul className="menu-list">
-            <li className="menu-item">
-              <div>
-                <img src="../../imgs/mm01.jpg" alt="" />
-              </div>
-              <div className="info-wrap">
-                <p className="menu-name">
-                  <strong>무뼈닭발</strong>
-                </p>
-                <p className="menu-name-en">boneless chicken feet</p>
-                <hr className="gray-line" />
-                <p className="menu-price">
-                  <data value="13000">13,000</data>원
-                </p>
-              </div>
-            </li>
-            <li className="menu-item">
-              <div>
-                <img src="../../imgs/mm01.jpg" alt="" />
-              </div>
-              <div className="info-wrap">
-                <p className="menu-name">
-                  <strong>무뼈닭발</strong>
-                </p>
-                <p className="menu-name-en">boneless chicken feet</p>
-                <hr className="gray-line" />
-                <p className="menu-price">
-                  <data value="13000">13,000</data>원
-                </p>
-              </div>
-            </li>
-          </ul>
-        </div>
-
-        <div>
-          <h4 className="menu-title">탕</h4>
-          <ul className="menu-list">
-            <li className="menu-item">
-              <div>
-                <img src="../../imgs/mm01.jpg" alt="" />
-              </div>
-              <div className="info-wrap">
-                <p className="menu-name">
-                  <strong>무뼈닭발</strong>
-                </p>
-                <p className="menu-name-en">boneless chicken feet</p>
-                <hr className="gray-line" />
-                <p className="menu-price">
-                  <data value="13000">13,000</data>원
-                </p>
-              </div>
-            </li>
-            <li className="menu-item">
-              <div>
-                <img src="../../imgs/mm01.jpg" alt="" />
-              </div>
-              <div className="info-wrap">
-                <p className="menu-name">
-                  <strong>무뼈닭발</strong>
-                </p>
-                <p className="menu-name-en">boneless chicken feet</p>
-                <hr className="gray-line" />
-                <p className="menu-price">
-                  <data value="13000">13,000</data>원
-                </p>
-              </div>
-            </li>
-          </ul>
-        </div>
-
-        <div>
-          <h4 className="menu-title">기타</h4>
-          <ul className="menu-list">
-            <li className="menu-item">
-              <div>
-                <img src="../../imgs/mm01.jpg" alt="" />
-              </div>
-              <div className="info-wrap">
-                <p className="menu-name">
-                  <strong>무뼈닭발</strong>
-                </p>
-                <p className="menu-name-en">boneless chicken feet</p>
-                <hr className="gray-line" />
-                <p className="menu-price">
-                  <data value="13000">13,000</data>원
-                </p>
-              </div>
-            </li>
-          </ul>
-        </div>
+            ))}
+          </>
+        )}
       </section>
     </MenuDiv>
   );
