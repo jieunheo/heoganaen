0cb868305bfbb27d9d533f192be97e4fdea0bec7
jieunheo <gjekdmlwl@naver.com>
2025-10-13 18:41:44 +0900

    메뉴 api 수정

--- back/api/getMenu.php
+++ back/api/getMenu.php
@@ -1,28 +1,42 @@
 <?php
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
-header("Access-Control-Allow-Headers: Content-Type");
+header("Access-Control-Allow-Origin: http://localhost:5173");
+header("Access-Control-Allow-Methods: GET");
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
 header("Content-Type: application/json; charset=UTF-8");
+
+ini_set('display_errors', 1);
 
 require_once 'config.php';
 
 try {
     $conn = getDBConnection();
+
+    $sql = "SELECT * FROM Category ORDER BY id ASC";
+    $result = $conn->query($sql);
+    
+    $cates = array();
+    if ($result->num_rows > 0) {
+        while($row = $result->fetch_assoc()) {
+            if($row['sub_name'] !== '') $cates['sub'][] = $row;
+            else $cates['main'][] = $row;
+        }
+    }
     
     $sql = "SELECT * FROM Menu WHERE is_visible = 1 ORDER BY 'order' DESC";
     $result = $conn->query($sql);
     
-    $data = array();
+    $menus = array();
     if ($result->num_rows > 0) {
         while($row = $result->fetch_assoc()) {
-            $data[] = $row;
+            $menus[$row['type']][] = $row;
         }
     }
     
     echo json_encode([
         "success" => true,
-        "data" => $data,
-        "count" => count($data)
+        "menus" => $menus,
+        "cates" => $cates,
+        "menu_count" => count($menus)
     ], JSON_UNESCAPED_UNICODE);
     
 } catch (Exception $e) {
